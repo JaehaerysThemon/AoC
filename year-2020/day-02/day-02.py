166cefac990a7fd34890e6d92b471b6cd3932910
leon-kleiber <leon.kleiber@students.fhnw.ch>
2022-12-02 00:22:33 +0100

    Year 2020 day 2

---
+++ year-2020/day-02/day-02.py
@@ -0,0 +1,24 @@
+def valid_password(path):
+    with open(path, 'r') as file:
+        count = 0
+        for string in file:
+            [pattern, password] = string.split(': ')
+            [min_rep, max_rep], char = pattern[:-2].split('-'), pattern[-1]
+            if int(min_rep) <= password.count(char) <= int(max_rep):
+                count += 1
+        return count
+
+
+def valid_password_2(path):
+    with open(path, 'r') as file:
+        counter = 0
+        for string in file:
+            [pattern, password] = string.split(': ')
+            [i1, i2], char = pattern[:-2].split('-'), pattern[-1]
+            if password[int(i1)-1] == char or password[int(i2)-1] == char:
+                counter += 1
+        return counter
+
+if __name__ == '__main__':
+    print(valid_password('./input'))
+    print(valid_password_2('./input'))
