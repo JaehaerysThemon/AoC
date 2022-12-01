8243ac747b27bab87d6fb6f418bcf81412cf55a3
leon-kleiber <leon.kleiber@students.fhnw.ch>
2022-12-01 23:52:41 +0100

    Year 2020 day 1

---
+++ year-2020/day-01/day-01.py
@@ -0,0 +1,32 @@
+def find_2020(path):
+    with open(path, 'r') as file:
+        prev = []
+        for num in file:
+            if 2020 - int(num) in prev:
+                return int(num) * (2020 - int(num))
+            prev.append(int(num))
+
+
+def find_x(x, numbers):
+    prev = []
+    for num in numbers:
+        if x-num in prev:
+            return [num, x-num]
+        prev.append(num)
+    return False
+
+
+def find_2020_3(path):
+    with open(path, 'r') as file:
+        prev = []
+        for num in file:
+            num = int(num)
+            if find_x(2020-num, prev):
+                [x1, x2] = find_x(2020-num, prev)
+                return num * x1 * x2
+            prev.append(num)
+
+
+if __name__ == '__main__':
+    print(find_2020('./input'))
+    print(find_2020_3('./input'))
--- year-2022/day-01/day01.py
+++ year-2022/day-01/day01.py
@@ -1,3 +1,14 @@
+def heaviest_loads(path):
+    with open(path, 'r') as file:
+        max_load, load = 0, 0
+        for food in file:
+            if food == '\n':
+                max_load, load = load if load > max_load else max_load, 0
+            else:
+                load += int(food)
+    return max_load
+
+
 def heaviest_3_loads(path):
     with open(path, 'r') as file:
         max_loads, load = [0, 0, 0], 0
@@ -12,4 +23,5 @@ def heaviest_3_loads(path):
 
 
 if __name__ == '__main__':
+    print(heaviest_loads('input'))
     print(sum(heaviest_3_loads('input')))
